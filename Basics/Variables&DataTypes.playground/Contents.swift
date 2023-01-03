89a0e4ac51596ce1bfa2af417ff122a116792d4e
Uriel Olascoaga <uriel1396@gmail.com>
2023-01-02 23:18:57 -0600

    Added data output functionality

--- Basics/Variables&DataTypes.playground/Contents.swift
+++ Basics/Variables&DataTypes.playground/Contents.swift
@@ -1,4 +1,18 @@
-var integerA: Int = 1
+var integerA: Int = 134
 var floatA: Float = 1.1
-var letterA: Character = "A"
-var boolean: Bool = false
+var doubleA: Double = 1.2
+var letterA: Character = "P"
+
+// Single print
+print("El valor de nuestro entero A es: \(integerA)")
+print("El valor de nuestro flotante A es: \(floatA)")
+print("El valor de nuestro doble A es: \(doubleA)")
+print("El valor de nuestro caracter A es: \(letterA)")
+
+// Multiple print
+print("""
+El valor de nuestro entero A es: \(integerA)
+El valor de nuestro flotante A es: \(floatA)
+El valor de nuestro doble A es: \(doubleA)
+El valor de nuestro caracter A es: \(letterA)
+""")
