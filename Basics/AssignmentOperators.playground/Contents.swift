5acc277b3676c786f80730313850b1abcd5f3d25
Uriel Olascoaga <uriel1396@gmail.com>
2023-01-04 23:54:18 -0600

    Added asignment operators exercise

---
+++ Basics/AssignmentOperators.playground/Contents.swift
@@ -0,0 +1,19 @@
+var value: Int = 13
+
+print("Operadores de asignación")
+print("El valor de la variable es: \(value)")
+
+value += 1
+print("El valor de la variable es: \(value)")
+
+value -= 2
+print("El valor de la variable es: \(value)")
+
+value *= 5
+print("El valor de la variable es: \(value)")
+
+value /= 4
+print("El valor de la variable es: \(value)")
+
+value %= 6
+print("El valor de la variable es: \(value)")
